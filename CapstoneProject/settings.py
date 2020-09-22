38c887b25b71aa37c5444139f88be30ad55edd5e
Jakeybaby <43561941+Jakeybaby@users.noreply.github.com>
2020-09-22 22:41:11 +1200

    Update settings.py

--- CapstoneProject/settings.py
+++ CapstoneProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -137,3 +137,5 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 GOOGLE_MAPS_API_KEY = 'AIzaSyCvTF4JKso7zjNZqQbpQugNRMmOUcnoiKQ'
+
+django_heroku.settings(locals())
