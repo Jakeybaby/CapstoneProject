ceeee809872562993636dd10f31ff69c2cd8a67a
Jakeybaby <43561941+Jakeybaby@users.noreply.github.com>
2020-09-22 22:27:24 +1200

    heroku push

--- CapstoneProject/settings.py
+++ CapstoneProject/settings.py
@@ -125,6 +125,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/account/static/'
 MEDIA_URL = '/images/'
 
